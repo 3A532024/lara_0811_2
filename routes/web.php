64a6866b76e586a877433ea28ce1c22b0d5fa7b3
3A532024 <kc1223127@gmail.com>
2018-11-01 10:12:35 +0800

    練習4-3

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@
 Route::get('welcome', function () {
     return view('welcome');
 });
-Route::get('hello/{name?}', ['as'=>'hello.index', function ($name='Everybody')  {
-    return 'Hello, '.$name;
+Route::get('say/{name?}', ['as'=>'hello.index', function ($name='Everybody') {    return 'Hello, '.$name;
 }]);
