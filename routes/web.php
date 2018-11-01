cc363dd7c6d33ca48be7b8e8dd4725fc304e135f
3A532024 <kc1223127@gmail.com>
2018-11-01 10:15:02 +0800

    練習5: 設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('welcome', function () {
 });
 Route::get('say/{name?}', ['as'=>'hello.index', function ($name='Everybody') {    return 'Hello, '.$name;
 }]);
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'dashboard';
+    });
+});
